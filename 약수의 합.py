814ccd5587f840e690d65ff0ce5070cef8b20385
Dowon Eathen Lee <96464835+mercuryfie@users.noreply.github.com>
2022-01-03 13:22:13 +0900

    Add files via upload

---
+++ 약수의 합.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    n_list = []
+    
+    for i in range(1, n+1):
+        if n % i == 0:
+            n_list.append(i)
+    
+    answer += sum(n_list)
+        
+    return answer
